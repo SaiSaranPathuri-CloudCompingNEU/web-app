5fdddb0e78f608131c83e16ca853787cb28a8107
Pathuri Sai Saran <99058160+SaiSaranPathuriNEU@users.noreply.github.com>
2023-02-01 20:15:41 -0500

    changes

--- test/app.test.js
+++ test/app.test.js
@@ -6,12 +6,12 @@ let expect = require("chai").expect;
 describe("/get testStatus", function () {
   it("Your App is working GOOD!!", async function () {
     let user = {
-      username: "demo@demo.com",
-      password: "test123",
-      firstname: "Alpha",
-      lastname: "Beta",
+      username: "test@gmail.com",
+      password: "Test123",
+      firstname: "Test",
+      lastname: "Test",
     };
-    const response = await request(app).get("/healthz").send(user);
+    const response = await request(app).get("/testStatus").send(user);
 
     expect(response.status).to.eql(200);
   });
