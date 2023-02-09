89fc6b7d51d1a99d02b9baae682a6a4374bdd9f4
Pathuri Sai Saran <99058160+SaiSaranPathuriNEU@users.noreply.github.com>
2023-02-09 02:48:13 -0500

    changes

--- models/users.js
+++ models/users.js
@@ -1,26 +1,16 @@
+module.exports = (Sequelize, DataTypes) => {
+  const User = Sequelize.define(
+    "User",
+    {
+      username: DataTypes.STRING,
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      freezeTableName: true,
+    }
+  );
 
-module.exports = (Sequelize, DataTypes) => { 
-
-    const User = Sequelize.define('User',{
-
-        username: DataTypes.STRING,
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-        password: DataTypes.STRING,
-    },{
-        freezeTableName: true
-    });
-
-return User;
-
-
-
-    
-}
-
-
-
-
-
-
-
+  return User;
+};
