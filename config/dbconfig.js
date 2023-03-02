e2226b453c05e54253972c479da5e2f0872aa69f
Pathuri Sai Saran <99058160+SaiSaranPathuriNEU@users.noreply.github.com>
2023-03-01 23:18:47 -0500

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,7 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "password", //S@1S@r@nPathuri
+  PASSWORD: "S@1S@r@nPathuri",
+  //PASSWORD: "password", //S@1S@r@nPathuri
   DATABASE: "cloud_app",
   DIALECT: "mysql",
 };
