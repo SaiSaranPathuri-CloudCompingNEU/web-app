4eb539d726328714fd85bf211403a49fff52d3b5
Pathuri Sai Saran <99058160+SaiSaranPathuriNEU@users.noreply.github.com>
2023-03-02 09:59:17 -0500

    changing bucket name

--- s3.js
+++ s3.js
@@ -1,12 +1,11 @@
 // const aws = require('aws-sdk');
 // const fs = require('fs');
 
-
 // const s3 = new aws.S3 ({
 //     region: "us-east-1",
 //     secretAccessKey: "AYuI8LSSauVgf1Xst2kTivF2BPclDdI9DpXFeLZm",
 //     accessKeyId: "AKIA5XTV4KA7E4X34646"
-    
+
 // })
 
 // function uploadFile(file){
@@ -16,23 +15,17 @@
 //         Bucket : "my-unique-bucket-62cb7f9a6334759e",
 //         Body: fileStream,
 //         Key: file.filename+Date.now().toString()
-        
+
 //     }
 //     return s3.upload(uploadParams).promise();
 // }
 // exports.uploadFile = uploadFile;
 
-
-
-
-function getFileStream (fileKey) {
-    
-const downloadParams = {
+function getFileStream(fileKey) {
+  const downloadParams = {
     Key: fileKey,
-    Bucket: "my-unique-bucket-62cb7f9a6334759e"
+    Bucket: "amazon-aws-bucket",
+  };
+  return s3.getObject(downloadParams).createReadStream();
 }
-return s3.getObject(downloadParams).createReadStream();
-
-
-}
-exports.getFileStream = getFileStream
+exports.getFileStream = getFileStream;
